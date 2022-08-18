6822f492b998d033ab7ed3fdb3f690d2e501bce8
Rami Elsayed <ramielsayed123@outlook.com>
2022-08-18 15:34:17 +0100

    server connected and installed bson

--- src/controllers/api/books.js
+++ src/controllers/api/books.js
@@ -1,23 +1,25 @@
- const { Book } = require("../../models/Book")
- const getAllBooks = (req, res) => {
-    try {
-        const data = await Book.find({});
-        return res.status(200).json(data);
-      } catch (error) {
-        console.log(`[ERROR]: Failed to get all books | ${error.message}`);
-        return res.status(500).json({ error: "Failed to get all books" });
-      }
- }
- const getOneBook = (req, res) => {
-    try {
+const { ObjectId } = require("bson");
+const { Book } = require("../../models");
+
+const getAllBooks = async (req, res) => {
+  try {
+    const data = await Book.find({});
+    return res.status(200).json({ data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all books | ${error.message}`);
+    return res.status(500).json({ error: "Failed to get all books" });
+  }
+};
+
+const getOneBook = async (req, res) => {
+  try {
     const { id } = req.params;
-      const data = await Book.findById(id);
-      return res.status(200).json(data);
-    } catch (error) {
-      console.log(`[ERROR]: Failed to get book | ${error.message}`);
-      return res.status(500).json({ error: "Failed to get book" });
-    }
- }
+    const data = await Book.findById(id);
+    return res.status(200).json({ data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get book | ${error.message}`);
+    return res.status(500).json({ error: "Failed to get book" });
+  }
+};
 
-
-module.exports = { getAllBooks, getOneBook}
+module.exports = { getAllBooks, getOneBook };
